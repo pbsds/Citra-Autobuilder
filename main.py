02b1e5e32d3f6468bcc441f5cb5812bbb13cb53f
pbsds <pbsds@hotmail.com>
2015-03-09 21:32:18 +0100

    Fixed CMake

--- main.py
+++ main.py
@@ -1,93 +1,112 @@
-import os, sys
+import os, sys, subprocess
 
-enable_QT = False
+g_enable_QT = False
 
-if os.path.exists("workspace"):
-	print "deleting old workspace..."
-	print os.system("rmdir /s /q workspace")
+g_git = None
+g_cmake = None
+g_mvs_compiler = None
 
-os.mkdir("workspace")
-os.mkdir("workspace\\build")
-#check if git is available:
-if not os.system("git version"):
-	git = "git"
-else:
-	for i in ("%PROGRAMFILES%", "%PROGRAMFILES(X86)%"):
-		if os.path.isfile(os.path.join(i, "Git\\bin\\git.exe")):
-			git = os.path.join(i, "Git\\bin\\git.exe")
-			break
-	else:
-		print "Git not found, exiting..."
-		sys.exit(1)
-#check if CMake is available:
-for i in ("%PROGRAMFILES%", "%PROGRAMFILES(X86)%"):
-	if os.path.isfile(os.path.join(i, "CMake\\bin\\cmake.exe")):
-		cmake = os.path.join(i, "CMake\\bin\\cmake.exe")
-		break
-else:
-	print "CMake not found, exiting..."
-	#cmake = "C:\Program Files (x86)\CMake\bin\cmake.exe"
-	sys.exit(1)
-#check if Microsoft Visual Studio 12.0 2013 is available:
-for i in ("%PROGRAMFILES%", "%PROGRAMFILES(X86)%"):
-	if os.path.isfile(os.path.join(i, "Microsoft Visual Studio 12.0 2013\\VC\\bin\\link.exe")):
-		MVScompiler = os.path.join(i, "Microsoft Visual Studio 12.0 2013\\VC\\bin")
-		break
-else:
-	print "Microsoft Visual Studio 12.0 2013 not found, exiting..."
-	#MVScompiler = "D:/Programs x32/Microsoft Visual Studio 12.0 2013/VC/bin/link.exe"
-	sys.exit(1)
+def Bool(i):
+	if i: return True
+	return False
+
+def Clean():
+	if os.path.exists("workspace"):
+		print "Deleting old workspace..."
+		if os.system("rmdir /s /q workspace"):
+			print "Unable to delete old workspace. Exiting..."
+			sys.exit(1)
+
+def FindDepencies():
+	global g_git, g_cmake, g_mvs_compiler
 	
+	#find depencies:
+	for i in ("%PROGRAMFILES%", "%PROGRAMFILES(X86)%", "C:\Program Files (x86)", "C:\Program Files"):
+		#git
+		check = os.path.join(i, "Git\\bin\\git.exe")
+		if os.path.exists(check) and os.path.isfile(check): g_git = check
+		#CMake
+		check = os.path.join(i, "CMake\\bin\\cmake.exe")
+		if os.path.exists(check) and os.path.isfile(check): g_cmake = check
+		#Microsoft Visual Studio 12.0 2013
+		check = os.path.join(i, "Microsoft Visual Studio 12.0 2013\\VC\\bin\\link.exe")
+		if os.path.exists(check) and os.path.isfile(check): g_mvs_compiler = os.path.split(check)[0]
+		
+		if g_git and g_cmake and g_mvs_compiler:
+			break
+	else:#error reporting
+		if not g_git and not os.system("git version"):
+			g_git = "git"
+		
+		if not g_git and g_cmake and g_mvs_compiler:
+			print "Error"
+			print ("Git, "*Bool(g_git) + "CMake, "*Bool(g_cmake) + "Microsoft Visual Studio 12.0 2013, "*Bool(g_mvs_compiler)) [:-2] + " not found!"
+			print "Exiting..."
+			sys.exit(1)
 
-
-#helpers:
 def FetchCitra():
-	global git
+	global g_git
 	prev = os.getcwd()
 	os.chdir("workspace")
 	
 	#clone
-	ret = os.system("%s clone --recursive https://github.com/citra-emu/citra.git" % git)
+	print "\nCloning citra repository..."
+	#print ("\"%s\" clone --recursive https://github.com/citra-emu/citra.git" % g_git)
+	ret = subprocess.call((g_git, "clone", "--recursive", "https://github.com/citra-emu/citra.git"))
+	#ret = os.system("\"%s\" clone --recursive https://github.com/citra-emu/citra.git" % g_git)
+	
 	if not os.path.exists("citra") or ret:
 		os.chdir(prev)
-		print "Couldn't clone citra repository! Exiting..."
+		print "\nCouldn't clone citra repository! Exiting..."
 		sys.exit(1)
 	
 	os.chdir(prev)
 
 def DoCMake():
-	global cmake, enable_QT, MVScompiler
+	global g_cmake, g_enable_QT, g_mvs_compiler
 	prev = os.getcwd()
-	os.chdir("workspace/build")
+	os.chdir("workspace\\build")
 	
-	#setup
-	
-	print "Generating CMake..."
-	if not os.system("\"%s\" \"%s\"" % (cmake, os.path.join(prev, "workspace\\citra"))):
+	#setup/generate
+	print "\nGenerating CMake..."
+	if subprocess.call((g_cmake, "-G", "Visual Studio 12 2013", os.path.join(prev, "workspace\\citra"))):
 		print "ech"
 	
 		#configure
-		
-		print "Configuring CMake..."
+		print "\nConfiguring CMake..."
 		f = open("CMakeCache.txt", "rb")
 		Cache = f.read().replace("\r\n", "\n").replace("\r", "\n").split("\n")
 		f.close()
 		
 		for i, line in enumerate(Cache):
 			if line[:15] == "ENABLE_QT:BOOL=":
-				Cache[i] = "ENABLE_QT:BOOL=" + ("ON" if enable_QT else "OFF")
-			if line[:22] == "CMAKE_LINKER:FILEPATH=":
-				Cache[i] = "CMAKE_LINKER:FILEPATH=" + os.path.join(MVScompiler, "link.exe").replace("\\", "/")
+				Cache[i] = "ENABLE_QT:BOOL=" + ("ON" if g_enable_QT else "OFF")
+			#if line[:22] == "CMAKE_LINKER:FILEPATH=":
+			#	Cache[i] = "CMAKE_LINKER:FILEPATH=" + os.path.join(g_mvs_compiler, "link.exe").replace("\\", "/")
 		
 		f = open("CMakeCache.txt", "w")
 		f.write("\n".join(Cache))
 		f.close()
 		
 		#Generate:
-		print "Generating CMake..."
-		if not os.system("\"%s\" \"%s\"" % (cmake, os.path.join(prev, "workspace\\citra"))):
-			print "CMake generation failed!"
+		print "\nGenerating CMake..."
+		if subprocess.call((g_cmake, "-G", "Visual Studio 12 2013", os.path.join(prev, "workspace\\citra"))):
+			print "\n\nCMake generation failed!"
+	
+	os.chdir(prev)
+
+def DoCompile():
+	pass
+	
 	
 #do:
+FindDepencies()
+
+Clean()
+os.mkdir("workspace")
+os.mkdir("workspace\\build")
+
 FetchCitra()
 DoCMake()
+
+#Clean()
