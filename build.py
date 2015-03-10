e7225ca204657f87ac47e271fafa3848b836b62a
pbsds <pbsds@hotmail.com>
2015-03-11 00:24:59 +0100

    started work on nightly builder. todo: make login work and compile at regular intervals

---
+++ build.py
@@ -0,0 +1,127 @@
+import os, sys, subprocess, atexit, shutil
+
+
+#todo: add Qt support, linux and possibly mac support aswell
+
+#settings:
+g_enable_QT = False
+
+#depencies paths
+g_git = None
+g_cmake = None
+g_mvs_link = None
+g_msbuild = None
+
+#helpers and init/cleanup
+def NotBool(i):
+	if i: return False
+	return True
+def Clean():
+	if os.path.isdir("workspace"):
+		print "Deleting old workspace..."
+		#if shutil.rmtree("workspace"):
+		if subprocess.call(("rmdir /s /q workspace"), shell = True):
+			print "Unable to delete old workspace. Exiting..."
+			sys.exit(1)
+def FindDepencies():
+	global g_git, g_cmake, g_mvs_link, g_msbuild
+	
+	#find depencies:
+	for i in (os.environ["ProgramFiles"], os.environ["ProgramFiles(x86)"], "C:\Program Files (x86)", "C:\Program Files"):
+		#git
+		check = os.path.join(i, "Git\\bin\\git.exe")
+		if os.path.exists(check) and os.path.isfile(check): g_git = check
+		#CMake
+		check = os.path.join(i, "CMake\\bin\\cmake.exe")
+		if os.path.exists(check) and os.path.isfile(check): g_cmake = check
+		#Microsoft Visual Studio 12.0 2013
+		check = os.path.join(i, "Microsoft Visual Studio 12.0 2013\\VC\\bin\\link.exe")
+		if os.path.exists(check) and os.path.isfile(check): g_mvs_link = os.path.split(check)[0]
+		#MSBUILD
+		check = os.path.join(i, "MSBuild\\12.0\\Bin\\MSBuild.exe")
+		if os.path.exists(check) and os.path.isfile(check): g_msbuild = check
+		
+		if g_git and g_cmake and g_mvs_link and g_msbuild:
+			break
+	else:#error reporting
+		if not g_git and not subprocess.call(("git", "version")):
+			g_git = "git"
+		
+		if not g_git or not g_cmake or not g_mvs_link or not g_msbuild:
+			print "Error"
+			print ("Git, "*NotBool(g_git) + "CMake, "*NotBool(g_cmake) + "Microsoft Visual Studio 12.0 2013, "*NotBool(g_mvs_link) + "MSBuild, "*NotBool(g_msbuild)) [:-2] + " not found!"
+			print "Exiting..."
+			sys.exit(1)
+
+#Do the work
+def FetchCitra():
+	global g_git
+	prev = os.getcwd()
+	os.chdir("workspace")
+	
+	#clone
+	print "\nCloning citra repository..."
+	ret = subprocess.call((g_git, "clone", "--recursive", "https://github.com/citra-emu/citra.git"))
+	
+	if not os.path.exists("citra") or ret:
+		os.chdir(prev)
+		print "\nCouldn't clone citra repository! Exiting..."
+		sys.exit(1)
+	
+	os.chdir(prev)
+def DoCMake():
+	global g_cmake, g_enable_QT, g_mvs_link
+	prev = os.getcwd()
+	os.chdir("workspace\\build")
+	
+	#setup/generate
+	print "\nGenerating CMake..."
+	if subprocess.call((g_cmake, "-G", "Visual Studio 12 2013", os.path.join(prev, "workspace\\citra"))):
+		print "ech"
+	
+		#configure
+		print "\nConfiguring CMake..."
+		f = open("CMakeCache.txt", "rb")
+		Cache = f.read().replace("\r\n", "\n").replace("\r", "\n").split("\n")
+		f.close()
+		
+		for i, line in enumerate(Cache):
+			if line[:15] == "ENABLE_QT:BOOL=":
+				Cache[i] = "ENABLE_QT:BOOL=" + ("ON" if g_enable_QT else "OFF")
+			#if line[:22] == "CMAKE_LINKER:FILEPATH=":
+			#	Cache[i] = "CMAKE_LINKER:FILEPATH=" + os.path.join(g_mvs_link, "link.exe").replace("\\", "/")
+		
+		f = open("CMakeCache.txt", "w")
+		f.write("\n".join(Cache))
+		f.close()
+		
+		#Generate:
+		print "\nGenerating CMake..."
+		if subprocess.call((g_cmake, "-G", "Visual Studio 12 2013", os.path.join(prev, "workspace\\citra"))):
+			print "\n\nCMake generation failed!"
+	
+	os.chdir(prev)
+def DoCompile():
+	global g_msbuild
+	prev = os.getcwd()
+	os.chdir("workspace\\build")
+	print "\nCompiling citra..."
+	if subprocess.call((g_msbuild, "/t:citra", "citra.sln")):
+		print "\nCompile failed!"
+		sys.exit(1)
+	os.chdir(prev)
+
+def Main():
+	FindDepencies()
+
+	Clean()
+	os.mkdir("workspace")
+	os.mkdir("workspace\\build")
+
+	FetchCitra()
+	DoCMake()
+	DoCompile()
+
+	shutil.make_archive("Citra", 'zip', "workspace\\build\\bin")
+if __name__ == "__main__":
+	Main()
---
+++ nightlybuild daemon.py
@@ -0,0 +1,196 @@
+import sys, time, subprocess, glob, os
+#requires that python2 and git is available in the PATH, or that they lie in their default install directory
+
+
+#globals:
+g_repository = None
+g_username = None
+g_password = None
+g_author = None
+
+g_template = None
+
+g_git = None
+g_python = None
+
+#helpers:
+def Say(text):
+	print time.strftime("[%H:%M:%S]"), text
+def FindPythonGit():
+	global g_git, g_python
+	
+	if not subprocess.call(("git", "version")):
+		g_git = "git"
+	else:
+		for i in (os.environ["ProgramFiles"], os.environ["ProgramFiles(x86)"], "C:\Program Files (x86)", "C:\Program Files"):
+			check = os.path.join(i, "Git\\bin\\git.exe")
+			if os.path.exists(check) and os.path.isfile(check):
+				g_git = check
+				break
+		else:
+			Say("Git not found, exiting...")
+			sys.exit(1)
+	
+	if not subprocess.call(("python", "--version")):
+		g_python = "python"
+	elif os.path.isfile("C:/Python27/python.exe"):
+		g_python = "C:/Python27/python.exe"
+	else:
+		Say("Python not found, exiting...")
+		sys.exit(1)
+def MakeIndexTableRow(link, size, commits, hash, success, debuglink, date):
+	for i, p in enumerate(("", "K", "M", "G", "T")):
+		if float(size)/(1024**i) >= 900: continue
+		size = "%.2f%sB" % (float(size)/(1024**i), p)
+		break
+		
+	link = "<a href=\"%s\">Download Citra.zip</a>" % link if success else "None"
+	debuglink = "<a href=\"%s\">output.log</a>" % debuglink
+	
+	success = ("Failed", "Successful")[1*success]
+	
+	if hash:
+		commits = "<a href=\"https://github.com/citra-emu/citra/commit/%s\">%s</a>" % (hash, commits)
+		hash = "<a href=\"https://github.com/citra-emu/citra/commit/%s\">%s</a>" % (hash, hash)
+	
+	return "\t\t\t<tr>\n%s\n\t\t\t</tr>" % ("\n".join(("\t\t\t\t<td>%s</td>" % i for i in (link, size, commits, hash, success, debuglink, date))))
+
+#doers
+def DoCompile():
+	global g_python
+	Say("Building citra...")
+	prev = os.getcwd()
+	
+	#import build
+	#build.Main()
+	
+	f = open("output.log", "wb")
+	if subprocess.call((g_python, "build.py"), stdout=f, stderr=f):#i hope this method will write to the file untill it crashes
+		Say("build failed!")
+		f.close()
+		#todo: find the git info on failed build
+		return False, ["output.log"], 0, ""
+	f.close()
+	
+	#get commitcount and hash
+	os.chdir("workspace")
+	os.chdir("citra")
+	try:
+		commits = int(subprocess.check_output(("git", "rev-list", "HEAD", "--count")))
+		hash = subprocess.check_output(("git", "rev-parse", "HEAD"))
+		if hash[-1] == "\n":
+			hash = hash[:-1]
+	except Exception as e:# subprocess.CalledProcessError:
+		Say("Unable to get has and commit count")
+		Say("\n"+str(e))
+		return False, ["output.log"], 0, ""
+	
+	os.chdir(prev)
+	return True, ["Citra.zip", "output.log"], commits, hash
+def AddToSite(success, files, commits, hash):
+	global g_repository, g_git
+	prev = os.getcwd()
+	if not os.path.exists("nightlybuild"): os.mkdir("nightlybuild")
+	os.chdir("nightlybuild")
+	
+	gitfolder = g_repository.split("/")[-1][:-4]
+	if os.path.isdir(gitfolder):
+		os.chdir(gitfolder)
+		subprocess.call((g_git, "pull"))
+	else:
+		subprocess.call((g_git, "clone", g_repository))
+		os.chdir(gitfolder)
+	
+	if not os.path.isdir("citra nightlies"): os.mkdir("citra nightlies")
+	os.chdir("citra nightlies")
+	
+	#if previous build of same version exists
+	if len(glob.glob("%i-%s-*" % (commits, hash))):
+		pass#remove citra and output.log
+		return
+	
+	#move to proper place:
+	dir = "%i-%s-%s-%s" % (commits, hash, success, time.strftime("%H-%M-%S-%d-%m-%Y"))
+	os.mkdir(dir)
+	for i in files:
+		os.rename(i, os.path.join(dir, os.path.basename(i)))
+	
+	#sort what i want:
+	buildlist = []
+	for folder in (os.path.basename(i) for i in glob.glob("*") if os.path.isdir(i)):
+		index, hash, success, H, M, S, d, m, Y = folder.split("-")
+		date = "%s:%s:%s %s/%s-%s" % (H, M, S, d, m, Y)
+		success = success == "True"
+		index = int(index)
+		link = "%s/Citra.zip" % folder
+		debuglink = "%s/output.log" % folder
+		
+		if success:
+			size = os.path.getsize(os.path.join(folder, "Citra.zip"))
+		else:
+			size = 0
+		
+		#filter here?
+		pass
+		
+		buildlist.append((index, MakeIndexTableRow(link, size, index, hash, success, debuglink, date)))
+	
+	#i want about 40?
+	pass
+	
+	#make index file
+	f = open("index.html", "wb")
+	f.write(g_template.replace("<!--SPLIT-->", "\n".join(map(lambda x: x[1], sorted(buildlist, lambda x: -x[0])))))
+	f.close()
+	
+	#commit:
+	os.chdir(os.path.join(prev, "nightlybuild", gitfolder))
+	subprocess.call((g_git, "add", "."))
+	subprocess.call((g_git, "commit", "--author=\"%s\"" % g_author, "-m", "Added build for citra commit number %i" % commits))#, "--dry-run"))
+	subprocess.call((g_git, "commit", "--author=\"%s\"" % g_author, "-m", "Added build for citra commit number %i" % commits))#, "--dry-run"))
+	
+	subprocess.call((g_git, "push"))
+	#cmd = subprocess.Popen((g_git, "push", "origin", "master"), stdin=subprocess.PIPE)
+	#time.sleep(5)
+	#cmd.stdin.write("%s\n"%g_username)
+	#time.sleep(1)
+	#cmd.stdin.write("%s\n"%g_password)
+	
+	#cmd.wait()
+	#while not cmd.poll():
+	#	time.sleep(0.2)
+	
+	os.chdir(prev)
+#daemon
+def Mainloop(usr, psw, author, repo):
+	global g_repository, g_username, g_password, g_author
+	g_repository = repo
+	g_username = usr
+	g_password = psw
+	g_author = author
+	
+	#while 1:
+	if 1:
+		success, files, commits, hash = DoCompile()
+		#success, files, commits, hash = True, ("Citra.zip", "output.log"), 123123, "asdasdasdasdasd"
+		if hash:
+			files = [os.path.join(os.getcwd(), i) for i in files]
+			AddToSite(success, files, commits, hash)
+		
+		
+#do:
+def Main():
+	global g_template
+	if len(sys.argv) < 5:
+		print "Usage: bleh.py <git username> <git password> <name <email>> <github.io repository>"
+	else:
+		usr, psw, author, repo = sys.argv[1:5]
+	
+	f = open("template.html", "rb")
+	g_template = f.read().replace("\r\n", "\n").replace("\r", "\n")
+	f.close()
+	
+	FindPythonGit()
+	Mainloop(usr, psw, author, repo)
+if __name__ == "__main__":
+	Main()
